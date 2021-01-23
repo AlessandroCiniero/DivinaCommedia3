8610aebf55c5efb53e951df01e2e1af865c4c91a
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-01-23 19:49:42 +0100

    progressi cutscene #1

---
+++ Assets/Scripts/Cutscene#1/cutscene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutscene : MonoBehaviour
+{
+    private float asd = 0f;
+    public GameObject virgilio;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        asd += Time.deltaTime;
+        Debug.Log(asd);
+
+        if (asd > 5) {
+            virgilio.transform.Translate(1, 1, 1);
+         }
+
+    }
+}
