d9ee3b60550cc7f3a6832cd36d6a16e06c8b6fcb
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-02-09 21:40:51 +0100

    prova pergamene, eretici

---
+++ Assets/Pergamene/immaginiPergamene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class immaginiPergamene : MonoBehaviour
+{
+    private Image _image; 
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public int asd;
+
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (asd == 0)
+            _image.sprite = sprite1;
+        else 
+            _image.sprite = sprite2;
+    }
+}
---
+++ Assets/Pergamene/pergamena.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class pergamena : MonoBehaviour
+{
+
+    public GameObject _pergamena;
+
+    public void Start() {
+
+        
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("e")) {
+            Debug.Log("toggle pergamena");
+            TogglePergamena();
+        }
+
+    }
+
+    public void TogglePergamena() {
+
+        if (_pergamena != null)
+        {
+            bool isActive = _pergamena.activeSelf;
+            _pergamena.SetActive(!isActive);
+        }
+    }
+}
