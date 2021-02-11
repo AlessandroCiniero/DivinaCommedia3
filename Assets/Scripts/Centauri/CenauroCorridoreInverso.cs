327430f06b5a60028c5e15a6d4051c43f31583b6
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-02-11 15:57:39 +0100

    Centauri sistemti e animati

--- Assets/Scripts/Centauri/CenauroCorridoreInverso.cs
+++ Assets/Scripts/Centauri/CenauroCorridoreInverso.cs
@@ -3,27 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-
-public class CenauroCorridore : MonoBehaviour
+public class CenauroCorridoreInverso : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       Sequence mySequence = DOTween.Sequence();
+        Sequence mySequence = DOTween.Sequence();
         mySequence
-        .Append(transform.DOMoveZ(transform.position.z - 500, 20).SetEase(Ease.Linear))
-        .Append(transform.DORotate(new Vector3(0, 0, 0), 1))
-        .Append(transform.DOMoveZ(transform.position.z, 17).SetEase(Ease.Linear))
+        .Append(transform.DOMoveZ(transform.position.z + 500, 20).SetEase(Ease.Linear))
         .Append(transform.DORotate(new Vector3(0, 180, 0), 1))
+        .Append(transform.DOMoveZ(transform.position.z, 17).SetEase(Ease.Linear))
+        .Append(transform.DORotate(new Vector3(0, 0, 0), 1))
         .SetLoops(-1);
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
+        
     }
 }
