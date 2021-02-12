72fa9e336638bba14a491aa604a11b435286fadb
Simone <restivosimone@gmail.com>
2021-02-12 17:19:55 +0100

    Caricati Script VirgilioIracondi FilippoArgenti e Flegias

---
+++ Assets/Scripts/Flegias.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+
+public class Flegias : Interactable
+{
+    private float distance;
+
+    public GameObject DialogueName;
+    public GameObject DialogueText;
+    public GameObject ContinueText;
+    public GameObject DanteController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = InteractionManager.distance;
+    }
+
+
+    public override void Interact(GameObject caller)
+    {
+
+        if (VirgilioIracondi.state == 0)
+        {
+            //Set State 0-->1
+            VirgilioIracondi.state++;
+
+
+
+            //fai partire barca
+
+            //quando la barca è arrivata state++
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/VirgilioIracondi.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+
+public class VirgilioIracondi : Interactable
+{
+    private float distance;
+    public static int state = 0;
+    public GameObject DialogueName;
+    public GameObject DialogueText;
+    public GameObject ContinueText;
+    public GameObject DanteController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = InteractionManager.distance;
+    }
+
+
+    public override void Interact(GameObject caller)
+    {
+        //Lock Interaction
+        InteractionManager.active = false;
+
+        //Lock rotation and movement
+        MouseLook.active = false;
+        PlayerMovement.active = false;
+
+        //Set animation rotation and movement
+
+        this.GetComponent<Animator>().Play("Idle");
+        this.GetComponent<NavMeshAgent>().enabled = false;
+        this.GetComponent<Movimento>().enabled = false;
+
+
+        //this.transform.LookAt(new Vector3(DanteController.transform.position.x, this.transform.position.y, DanteController.transform.position.y));
+
+        if (state == 0) //bisogna interagire con  Flegias
+        {
+            DialogueName.GetComponent<Text>().text = "VIRGILIO";
+
+            DialogueText.GetComponent<Text>().text = "Finalmente ti sei ridestato, Dante. Continueremo il nostro viaggio sulla barca di Flegiàs, per attraversare la palude Stige. Interagisci con lui quando sei pronto a salpare. Premi E per visualizzare la tua pergamena. Ricordati che potremo accedere al cerchio successivo solo quando avrai trovato tutti i versi mancanti. Li completerai parlando con i dannati.";
+
+            ContinueText.GetComponent<Text>().text = "Clicca per continuare.";
+        }
+
+        //state 1 interagito con flegias parte la barca e si ferma quando si trova filippo argenti
+
+        if (state == 1) //parlato con chirone, bisogna parlare con nesso
+        {
+            DialogueName.GetComponent<Text>().text = "VIRGILIO";
+
+            DialogueText.GetComponent<Text>().text = "Ormai, figliuolo, si avvicina la città chiamata Dite, coi suoi afflitti abitanti, col grande stuolo di diavoli.";
+
+            ContinueText.GetComponent<Text>().text = "Clicca per continuare.";
+        }
+
+        //state 2
+        if (state == 2) //barca ferma bisogna parlare con filippo argenti
+        {
+            DialogueName.GetComponent<Text>().text = "VIRGILIO";
+
+            DialogueText.GetComponent<Text>().text = "Ormai, figliuolo, si avvicina la città chiamata Dite, coi suoi afflitti abitanti, col grande stuolo di diavoli.";
+
+            ContinueText.GetComponent<Text>().text = "Clicca per continuare.";
+        }
+
+
+
+  
+
+
+        //Left Click to Continue
+
+        StartCoroutine(WaitForLeftClick());
+
+
+
+        //StartCoroutine(ResetChat());
+    }
+
+    IEnumerator ResetChat()
+    {
+        yield return new WaitForSeconds(5f);
+        DialogueName.GetComponent<Text>().text = "";
+        DialogueText.GetComponent<Text>().text = "";
+
+        InteractionManager.active = true;
+        MouseLook.active = true;
+        PlayerMovement.active = true;
+
+        //Reset animation and movement
+        //this.GetComponent<Animator>().Play("Standard Walk");
+        this.GetComponent<NavMeshAgent>().enabled = true;
+        this.GetComponent<Movimento>().enabled = true;
+
+    }
+
+    IEnumerator WaitForLeftClick()
+    {
+        yield return new WaitForSeconds(1f);
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                DialogueName.GetComponent<Text>().text = "";
+                DialogueText.GetComponent<Text>().text = "";
+                ContinueText.GetComponent<Text>().text = "";
+
+                InteractionManager.active = true;
+                MouseLook.active = true;
+                PlayerMovement.active = true;
+
+                //Reset animation and movement
+                //this.GetComponent<Animator>().Play("Standard Walk");
+                this.GetComponent<NavMeshAgent>().enabled = true;
+                this.GetComponent<Movimento>().enabled = true;
+
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+}
