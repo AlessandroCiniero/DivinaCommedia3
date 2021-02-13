7f6718ead8d4f3a97fc2cab6fc65aa03793b92c8
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-02-14 00:14:45 +0100

    modifica menu iniziale

---
+++ Assets/scrittaIniziale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class scrittaIniziale : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+        yield return new WaitForSeconds(6);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+}
