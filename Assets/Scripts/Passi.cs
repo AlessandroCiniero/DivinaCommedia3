93674e2fc37c9fa23e345f9d2d9ca73127a4cb86
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-02-14 23:20:14 +0100

    inseriti suoni passi, dissolvenze scene e passaggio automatico iracondi -> cutscene

---
+++ Assets/Scripts/Passi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passi : MonoBehaviour
+{
+    AudioSource _passo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _passo = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
+        
+        {
+            _passo.Play();
+        }
+       else if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("s") || Input.GetKeyUp("d"))
+       
+        {
+            if (!(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")))
+            _passo.Stop();
+        }
+    }
+}
--- Assets/Scripts/SaliSuBarca.cs
+++ Assets/Scripts/SaliSuBarca.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaliSuBarca : MonoBehaviour
 {
     private bool asd;
+    public GameObject boat;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,13 @@ public class SaliSuBarca : MonoBehaviour
             Debug.Log("Dante sale");
             asd = false;
             transform.position = new Vector3(695.42f, 3.2f, 864.45f);
+        }
+
+
+        //CAMBIO SCENA
+        if (boat.transform.position.x < 330 && VirgilioIracondi.state == 4)
+        {
+            SceneManager.LoadScene("Cutscene#1");
         }
     }
 }
