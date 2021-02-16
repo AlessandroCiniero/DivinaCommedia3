eb9b28ad10966d2c1b780bf023f491118b29c2bd
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-02-16 12:11:25 +0100

    prova video intro

---
+++ Assets/Menu/SkipVideo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SkipVideo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+        yield return new WaitForSeconds(12);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+
+            SceneManager.LoadScene("Iracondi_scena");
+
+        }
+    }
+}
