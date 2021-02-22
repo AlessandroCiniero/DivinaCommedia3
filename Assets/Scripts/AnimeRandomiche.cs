2220609f87df5b8b53c6c976032302e808d3adfe
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-02-22 19:02:10 +0100

    anime randomizzate, post processing fatta in ogni scena

---
+++ Assets/Scripts/AnimeRandomiche.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimeRandomiche : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator _anim;
+
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+        _anim = GetComponent<Animator>();
+
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+        yield return new WaitForSeconds(Random.Range(5, 15));
+
+        _anim.SetInteger("a", Random.Range(0, 3));
+
+        StartCoroutine(ExampleCoroutine());
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
